809aa464a03fb0e50319470584aef236e896e917
dulitharuvin <dulitha.ruvin@gmail.com>
2023-07-22 00:26:43 +0200

    header structure added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,20 @@
 import React, { Fragment } from "react";
 
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
 const Header = (props) => {
-  return <Fragment>
-    <header>
+  return (
+    <Fragment>
+      <header className={classes.header}>
         <h1>React Meals</h1>
         <button>Cart</button>
-    </header>
-    <div>
-        <img/>
-    </div>
-  </Fragment>;
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
