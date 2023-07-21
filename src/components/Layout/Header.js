e12930661114abfe79688b0b9ac22b3e2d762597
dulitharuvin <dulitha.ruvin@gmail.com>
2023-07-22 00:21:11 +0200

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const Header = (props) => {
+  return <Fragment>
+    <header>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+    </header>
+    <div>
+        <img/>
+    </div>
+  </Fragment>;
+};
+
+export default Header;
