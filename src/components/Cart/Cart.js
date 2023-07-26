efbafcb6d557c0a4f11650e7e16968dcb5d10a08
dulitharuvin <dulitha.ruvin@gmail.com>
2023-07-27 00:10:51 +0200

    cart modal show hide logic implemented

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,14 +20,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["btn--alt"]}>Class</button>
+        <button className={classes["btn--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
